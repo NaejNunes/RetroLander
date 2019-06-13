9b88883974c033c8b65f0a0ac3ba8befd098adcb
NaejNunes <jeannobreganunes@hotmail.com>
2019-06-12 22:19:07 -0300

    Iniciando a movimentacao do player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform player;
+
+    public float zoomMinimo = 15f;
+    public float ZoomMximo = 4f;
+    public float intervalo = 0.1f;
+    private Camera minhaCamera;
+    private float velocidadeZoom;
+    private Vector3 velocidadeMovimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minhaCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            if (player.position.y > 0)
+            {
+                minhaCamera.orthographicSize = Mathf.SmoothDamp(minhaCamera.orthographicSize, zoomMinimo, ref velocidadeZoom, intervalo);
+            }
+            else {
+                Vector3 novaPosicao = new Vector3(player.position.x, player.position.y, -10);
+                transform.position = Vector3.SmoothDamp(transform.position, novaPosicao, ref velocidadeMovimento, intervalo);
+                minhaCamera.orthographicSize = Mathf.SmoothDamp(minhaCamera.orthographicSize, ZoomMximo, ref velocidadeZoom, intervalo);
+            }
+        }
+    }
+}
